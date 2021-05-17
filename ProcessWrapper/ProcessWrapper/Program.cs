417a715f0d3baae1c6aad59e5d011b56f5518835
TheoChristo <theofilos_xri@yahoo.gr>
2021-05-18 00:28:17 +0300

    Added Affinity (processor) selection for each process

--- ProcessWrapper/ProcessWrapper/Program.cs
+++ ProcessWrapper/ProcessWrapper/Program.cs
@@ -7,28 +7,36 @@ namespace ProcessWrapper
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Hello from ProcessWrapper!");
-            Console.WriteLine("Starting Console App...");
-            String appdir = @"C:\Users\cluster\Desktop\t\testRun\";
-            StartProcess(appdir);
+
+            StartProcess(@"C:\Users\cluster\Desktop\t\testRun\", new int[] {4,5});
+
             Console.WriteLine("Exiting programm..");
         }
 
-        private static void StartProcess(String appdir)
+        private static void StartProcess(String appdir, int[] processors)
         {
+            Console.WriteLine("Starting Console App at"+appdir);
             Process process = new Process();
             process.StartInfo.WorkingDirectory = appdir;
-            process.StartInfo.FileName = appdir + @"Parallel_Tumor_ModellingConsoleApp.exe";
-            process.StartInfo.Arguments = appdir + @"inputParams.txt";
-
-            //long AffinityMask = (long)process.ProcessorAffinity;
-            ////AffinityMask &= 0x000F; // use only any of the first 4 available processors
-            ////process.ProcessorAffinity = (IntPtr)AffinityMask;
-
+            process.StartInfo.FileName = appdir + "Parallel_Tumor_ModellingConsoleApp.exe";
+            process.StartInfo.Arguments = appdir + "inputParams.txt";
             process.Start();
-            process.WaitForExit();// Waits here for the process to exit.
-            Console.WriteLine("Process Finished.");
+            
+            //Set Affinity
+            int bitMask = 0;
+            foreach (var item in processors)
+            {
+                bitMask = bitMask | (1 << item - 1);
+                Console.WriteLine("Processor #{0} was selected for affinity.", item);
+            }
+            long AffinityMask = (long)process.ProcessorAffinity;
+            AffinityMask &= bitMask;
+            process.ProcessorAffinity = (IntPtr)AffinityMask;
+
+            process.WaitForExit();
         }
+
+     
     }
 }
