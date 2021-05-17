16ecdc421173c4682720f155c93ae9e7f9f53f8d
TheoChristo <theofilos_xri@yahoo.gr>
2021-05-18 00:59:15 +0300

    Refactoring to incorporate Process Tupples

--- ProcessWrapper/ProcessWrapper/Program.cs
+++ ProcessWrapper/ProcessWrapper/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace ProcessWrapper
 {
     class Program
     {
+        
+        private static Tuple<String, int[]>[] procDic =
+        { //procDic(exeLocation, processorsToUse)
+            new Tuple<string, int[]>(@"C:\Users\cluster\Desktop\t\testRun1\", new int[] {1, 2}),
+            new Tuple<string, int[]>(@"C:\Users\cluster\Desktop\t\testRun2\", new int[] {3, 4})
+        };
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello from ProcessWrapper!");
 
-            StartProcess(@"C:\Users\cluster\Desktop\t\testRun\", new int[] {4,5});
+            StartProcess(procDic[0].Item1, procDic[0].Item2);
+
+            StartProcess(procDic[1].Item1, procDic[1].Item2);
 
             Console.WriteLine("Exiting programm..");
         }
@@ -34,7 +44,7 @@ namespace ProcessWrapper
             AffinityMask &= bitMask;
             process.ProcessorAffinity = (IntPtr)AffinityMask;
 
-            process.WaitForExit();
+            //process.WaitForExit();
         }
 
      
