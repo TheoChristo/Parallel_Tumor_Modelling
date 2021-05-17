db81824d41da4626c0a7f8851a62d3c37e9f8e1f
TheoChristo <theofilos_xri@yahoo.gr>
2021-05-17 23:13:39 +0300

    Created process starter function

--- ProcessWrapper/ProcessWrapper/Program.cs
+++ ProcessWrapper/ProcessWrapper/Program.cs
@@ -7,21 +7,28 @@ namespace ProcessWrapper
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello from ProcessWrapper!");
+            Console.WriteLine("Starting Console App...");
+            String appdir = @"C:\Users\cluster\Desktop\t\testRun\";
+            StartProcess(appdir);
+            Console.WriteLine("Exiting programm..");
+        }
+
+        private static void StartProcess(String appdir)
+        {
             Process process = new Process();
-            process.StartInfo.FileName = "Parallel_Tumor_ModellingConsoleApp.dll";
-            process.StartInfo.Arguments = "inputParams.txt";
+            process.StartInfo.WorkingDirectory = appdir;
+            process.StartInfo.FileName = appdir + @"Parallel_Tumor_ModellingConsoleApp.exe";
+            process.StartInfo.Arguments = appdir + @"inputParams.txt";
 
             //long AffinityMask = (long)process.ProcessorAffinity;
             ////AffinityMask &= 0x000F; // use only any of the first 4 available processors
             ////process.ProcessorAffinity = (IntPtr)AffinityMask;
 
-            //ProcessThread Thread = process.Threads[0];
-            //AffinityMask = 0x0002; // use only the second processor
-            //Thread.ProcessorAffinity = (IntPtr)AffinityMask;
-            ////process.Start();
-            ////process.WaitForExit();// Waits here for the process to exit.
-
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+            Console.WriteLine("Process Finished.");
         }
     }
 }
